924f3c74547891247785648c2d51655be417b461
Kae Benton <sonja.k.benton@gmail.com>
2020-08-07 15:53:55 -0400

    cards showing on page.

--- components/Cards.js
+++ components/Cards.js
@@ -21,7 +21,6 @@
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 const cardContainer = document.querySelector('.cards-container');
-
 function cardMaker(articleObj) {
     // creating elements 
     const card = document.createElement('div');
@@ -31,10 +30,10 @@ function cardMaker(articleObj) {
     const img = document.createElement('img');
     const authorName = document.createElement('span');
     // setting class names 
-    card.className = 'card';
-    headline.className = 'headline';
-    authorDiv.className = 'author';
-    imgCont.className = 'img-continer';
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgCont.classList.add('img-container');
     // setting attributes
     img.src = articleObj.authorPhoto;
     // setting text content 
@@ -46,16 +45,24 @@ function cardMaker(articleObj) {
     authorDiv.appendChild(imgCont);
     authorDiv.appendChild(authorName);
     imgCont.appendChild(img);
-
+    // setting click events
+    card.addEventListener('click', (event) => {
+        console.log(articleObj.headline);
+    })
     return card;
 }
 
-
-
-// axios.get('https://lambda-times-api.herokuapp.com/articles')
-// .then(response => {
-    
-// })
-// .catch(error => {
-//     console.log(error.message); // an error would appear a message 
-// });
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => { 
+        const articleTopics = Object.values(response.data.articles);
+        articleTopics.forEach(topic => {
+        topic.forEach(item => {
+        const articleName = cardMaker(item);
+        cardContainer.appendChild(articleName)
+        })
+    })
+})
+    .catch(error => {
+        error = console.log('error');
+        // console.log('Error: ', error.message); // an error would appear a message 
+})
